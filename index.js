4c15945c9211ba5247609f111f31a5976b7878ff
Sumanga Wimaladasa <136235586+sumaInovation@users.noreply.github.com>
2024-11-17 22:58:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,32 +8,46 @@ const server = require('http').createServer(App);
 const wss = new WebSocket.Server({ server });
 const dotenv = require('dotenv');
 dotenv.config();
+const CREDENTIALS={
+  type:process.env.type,
+  project_id:process.env.project_id,
+  private_key_id:process.env.private_key_i,
+  private_key:process.env.private_key,
+  client_email:process.env.client_email,
+  client_id:process.env.client_id,
+  auth_uri:process.env.auth_uri,
+  token_uri:process.env.token_uri,
+  auth_provider_x509_cert_url:process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url:process.env.client_x509_cert_url,
+  universe_domain:process.env.universe_domain
+
+
+}
 const PORT=process.env.PORT
-const SHEET_ID =process.env.GOOGLE_SHEETS_ID;
-const config={
-    
-    "type": process.env.type,
-    "project_id": process.env.project_id,
-    "private_key_id":process.env.private_key_id ,
-    "private_key":process.env.private_key ,
-    "client_email":process.env.client_email,
-    "client_id":process.env.client ,
-    "auth_uri": process.env.auth_uri,
-    "token_uri":process.env.token_uri ,
-    "auth_provider_x509_cert_url":process.env.auth_provider_x509_cert_url ,
-    "client_x509_cert_url":process.env.client_x509_cert_url ,
-    "universe_domain":process.env.universe_domain
-  }
-  
-  const auth = new google.auth.GoogleAuth({
-    credentials: config,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+const SHEET_ID = process.env.GOOGLE_SHEETS_ID;
+var Name,Email,Message
+const auth = new google.auth.GoogleAuth({
+  credentials: CREDENTIALS,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
-   const sheets = google.sheets({ version: 'v4', auth })
-    
-  
 
+async function WriteDataOnGoogleSheet() {
+Name="Jaami";
+Email="None";
+Message="Test1";
+    // Write row(s) to spreadsheet
+    const sheets = google.sheets({ version: 'v4', auth });
+    sheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId:SHEET_ID,
+    range: "Sheet1!A:B",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[Name, Email,Message]],
+    }})
+}
 
+//accessGoogleSheet();
 
 
 
@@ -53,17 +67,8 @@ wss.on('connection', (ws) => {
     if (client !== ws && client.readyState === WebSocket.OPEN) {
       client.send(message.toString());
     }});
-    if(message.toString()=="write"){
-        WriteDataOnGoogleSheet()
-    }
-    if(message.toString()=="create"){
-        CretaeNewSheet(); 
-    }
-    if(message.toString()=="read"){
-        readSheet('Sheet1!A1:C3')
-    }
-
-      
+    WriteDataOnGoogleSheet();
+    
   });
 
   // Event: When the WebSocket client disconnects
@@ -79,68 +84,57 @@ wss.on('connection', (ws) => {
 server.listen(PORT, () => {
   console.log(`HTTP and WebSocket server is running on http://localhost:${PORT}`);
 });
-//********************GOOGLE SHEET API PART */
-//Write data
-var Name,Email,Message
-async function WriteDataOnGoogleSheet() {
-Name="sumanga";
-Email="None";
-Message="Test1";
- 
-     // Write row(s) to spreadsheet
-    sheets.spreadsheets.values.append({
-    auth,
-    spreadsheetId:SHEET_ID,
-    range: "Sheet2!A:B",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-      values: [[Name, Email,Message]],
-    }})  
+
+
+
+
+async function FetchData(RANGE) {
+  const authClient = await auth.getClient();
+  const sheets = google.sheets({ version: 'v4', auth: authClient });
+
+  try {
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId:SHEET_ID,
+      range: RANGE,
+    });
+
+    const rows = res.data.values;
+    return rows
+  } catch (err) {
+    console.error('The API returned an error: ' + err);
+  }
 }
 
 
-// Asynchronous function to read data from a Google Sheet.
-async function readSheet(RANGE) {
+// Read Data From Sheet
+async function GetData(RANGE) {
+  const rows=await FetchData(RANGE);
+  if (rows.length) {
+    console.log('Data from the sheet:');
+    rows.forEach((row) => {
+      console.log(row);
+    });
+  } else {
+    console.log('No data found.');
+  }
+}
+async function SerchData(SEARCH_TERM,RANGE) {
+  const rows=await FetchData(RANGE);
+  if (rows.length) {
+    // Search for the term in the rows
+    const foundRows = rows.filter((row) => row.some((cell) => cell && cell.toLowerCase().includes(SEARCH_TERM.toLowerCase())));
 
-    try {
-        const response = await sheets.spreadsheets.values.get({
-            spreadsheetId:SHEET_ID, range:RANGE
-        });
-        const rows = response.data.values;  // Extracts the rows from the response.
-        console.log(rows)
-    } catch (error) {
-        console.error('error', error);  // Logs errors.
+     if (foundRows.length) {
+      console.log('Found matching rows:');
+      foundRows.forEach((row, index) => {
+        console.log(`Row ${index + 1}:`, row);
+      });
+    } else {
+       console.log('No matching data found.');
     }
-}
+  } else {
+    console.log('No data found.');
+  }
 
 
-
-// Cretyae new sheet
-async function CretaeNewSheet() {
-
-const requests={
-    spreadsheetId:SHEET_ID,
-    requestBody: {
-        requests: [
-                  {
-                    addSheet: {
-                                properties: {
-                                    title: 'JANUARY',
-                                            }
-                              }
-                   }
-                 ]
-                }
-             }
-
-try {
-    const client = await auth.getClient();
-    const response = await sheets.spreadsheets.batchUpdate(requests, { auth:client });
-    console.log('New sheet added');
-
-} catch (error) {  
-console.error('Error adding sheet:', error);
-
-}  
-                    
 }
