d73aaed9d2333e06c297dbaeb5342f5124b77eff
sumaautomationLK <sumanga0000@gmail.com>
2024-12-25 00:31:59 +0530

    updates

--- Writedata.js
+++ Writedata.js
@@ -1,19 +1,19 @@
 const dotenv = require('dotenv');
 const { google } = require('googleapis');
-
+const dayjs = require('dayjs');
 dotenv.config();
-const CREDENTIALS={
-  type:process.env.type,
-  project_id:process.env.project_id,
-  private_key_id:process.env.private_key_i,
-  private_key:process.env.private_key,
-  client_email:process.env.client_email,
-  client_id:process.env.client_id,
-  auth_uri:process.env.auth_uri,
-  token_uri:process.env.token_uri,
-  auth_provider_x509_cert_url:process.env.auth_provider_x509_cert_url,
-  client_x509_cert_url:process.env.client_x509_cert_url,
-  universe_domain:process.env.universe_domain
+const CREDENTIALS = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_i,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain
 
 
 }
@@ -32,48 +32,38 @@ const auth = new google.auth.GoogleAuth({
 
 async function WriteDataOnGoogleSheet(data) {
   console.log('involked');
-  var RANGE="Sheet1"
-  const currentDate = new Date();
-  // Get the full year, month, and day
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');  // Get month (0-11) and pad with zero
-    const day = String(currentDate.getDate()).padStart(2, '0');  // Pad day with zero if necessary
-    // Format the date as YYYY/MM/DD
-    const formattedDate = `${year}/${month}/${day}`;
-     const {start,end,reason}=await data;
-    
-    const timeArray1 = start.split(':');
-    const timeArray = end.split(':');
-    const timediference=(parseInt(timeArray[0],10)*3600+parseInt(timeArray[1],10)*60+parseInt(timeArray[2],10))-(parseInt(timeArray1[0],10)*3600+parseInt(timeArray1[1],10)*60+parseInt(timeArray1[2],10))
-     const   keys=[
+  var RANGE = "Sheet1"
+  const{start,end,reason}=data;
+  const time1Obj1 = new Date(`1970-01-01T${start}Z`);
+  const time1Ob2 = new Date(`1970-01-01T${end}Z`);
+  const diffInSeconds = (time1Ob2 - time1Obj1) / 1000;
 
-        [
-          formattedDate,
-          start,
-          end,
-          timediference,
-          reason
-        ]
-       ]
-     
+  const keys = [
 
-    
-      
-  const requestBody={
-    values:keys
+    [
+      new Date().toLocaleDateString(),
+      start,
+      end,
+      diffInSeconds,
+      reason
+    ]
+  ]
+
+ const requestBody = {
+    values: keys
   }
 
-    
-        // Write row(s) to spreadsheet
-        const sheets = google.sheets({ version: 'v4', auth });
-        sheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId:SHEET_ID,
-        range: RANGE,
-        valueInputOption: "USER_ENTERED",
-        requestBody: requestBody,
-      })
-      
-    }
-    
-    module.exports={WriteDataOnGoogleSheet}
+
+  // Write row(s) to spreadsheet
+  const sheets = google.sheets({ version: 'v4', auth });
+  sheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId: SHEET_ID,
+    range: RANGE,
+    valueInputOption: "USER_ENTERED",
+    requestBody: requestBody,
+  })
+
+}
+
+module.exports = { WriteDataOnGoogleSheet }
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const http = require('http');
 const WebSocket = require('ws');
 const cors = require('cors');
 const { WriteDataOnGoogleSheet } = require('./Writedata');
-const { Filterdata } = require('./Filterdata');
-const { mkdirSync } = require('fs');
+const { FetchData } = require('./Fetchdatas');
+const { json } = require('body-parser');
 const PORT = 5000;
 // Create an Express app
 const app = express();
@@ -36,14 +36,20 @@ wss.on('connection', async (ws) => {
   ws.on('message', async (message) => {
        try{
         const dataFromClient = JSON.stringify(message.toString());
+        const{reason}=JSON.parse(JSON.parse(dataFromClient));
+        //Updates Googlesheet when Any state change
+        if(reason!=undefined){
+          WriteDataOnGoogleSheet(JSON.parse(JSON.parse(dataFromClient)))
+        }
        
+        
         wss.clients.forEach((client) => {
          // Exclude the client that sent the message (skip the sender)
          if (client !== ws && client.readyState === WebSocket.OPEN) {
            client.send(dataFromClient);
            
          }
-         console.log(message.toString())
+        //  console.log(message.toString())
        })
         
        }catch(e){
@@ -58,13 +64,28 @@ wss.on('connection', async (ws) => {
   });
 });
 
-//Handle Post reques
-app.post('/', async (req, res) => {
-  const { starttime, endtime } = req.body; // Extract parameters from the request body
-  const result = await Filterdata("Sheet1", starttime, endtime)
-  console.log(result)
-  res.json(result);
 
+
+//Updates today for Piechart 
+app.post('/getdata',async(req,res)=>{
+  const{start,end,day}=req.body;
+  const googlesheetdata=await FetchData("Sheet1");
+   
+  if(day!=undefined){
+   res.json(
+    googlesheetdata.map(item=>{
+      if(item[0]==day)return item
+    })
+   )
+  }
+  if(start!=undefined){
+    const startTimestamp = new Date(start).getTime();
+    const endTimestamp = new Date(end ).getTime();
+    res.json(googlesheetdata.map(item=>{
+      if (new Date(item[0]).getTime() >= startTimestamp && new Date(item[0]).getTime() <= endTimestamp)return item
+    }))
+  }
+                                                          
 })
 
 // Start the HTTP server on port 3000
