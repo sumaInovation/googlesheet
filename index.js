43cda152facebb40a17b28b022c3d64de8a62c87
sumaautomationLK <sumanga0000@gmail.com>
2025-01-06 03:29:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ app.use((req, res, next) => {
 })
 
 app.use('/user',User);
-const corsOptions = {
-  origin: "*", // Replace with your frontend's origin
-  credentials: true, // Allow cookies to be sent in cross-origin requests
-};
+// const corsOptions = {
+//   origin: "*", // Replace with your frontend's origin
+//   credentials: true, // Allow cookies to be sent in cross-origin requests
+// };
          
-app.use(cors(corsOptions)); // Enable CORS with specified options
-// Middleware to parse incoming JSON data
+// app.use(cors(corsOptions)); // Enable CORS with specified options
+// // Middleware to parse incoming JSON data
 
 
 
