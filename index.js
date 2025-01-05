9fc798465e99af0d49e6b3621f0eb9f02577fcdc
sumaautomationLK <sumanga0000@gmail.com>
2025-01-06 05:02:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors({ origin: 'https://pptinovation.vercel.app', credentials: true }));
 app.use((req,res,next)=>{
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-
+next();
 
 });
 
