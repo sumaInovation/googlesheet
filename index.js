1bedc6fc9c3f51f080e8c706366e4b50858b82bf
sumaautomationLK <sumanga0000@gmail.com>
2025-01-06 06:44:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ app.use(express.urlencoded({ extended: true })); // Opti
 
 app.use(cors({ origin: 'https://pptinovation.vercel.app', credentials: true })); // Allow specific origin and credentials
 app.use('/user',User);
-
+app.use(cors({
+  origin:'*'
+}))
   
 const server = http.createServer(app);
 
