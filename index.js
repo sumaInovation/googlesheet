fd0c8dc327b946040f0dcf70191b3606568a6e80
sumaautomationLK <sumanga0000@gmail.com>
2025-01-08 21:06:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -212,15 +212,13 @@ res
 })
 
 app.get('/profile',(req,res)=>{
-  // console.log('Cookies:', req.cookies.token); // All cookies are available here
-  // res.json(req.cookies.token);
-  // jwt.verify(req.cookies.token,SECRET_KEY,(err,decode)=>{
-  //   if(err){
-  //     return res.status(401).json({"message":"connot veryfy"})
-  //   }
-  //   return res.json(decode)
-  // })
-  res.send("hi suma")
+  
+  jwt.verify(req.cookies.token,SECRET_KEY,(err,decode)=>{
+    if(err){
+      return res.status(401).json({"message":"connot veryfy"})
+    }
+    return res.json(decode)
+  })
 })
 app.listen(5000,()=>console.log('sever is running on port:5000'));
 
