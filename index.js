320b7a8e86b3871432061d07a8be955111173ba2
sumaautomationLK <sumanga0000@gmail.com>
2025-01-11 10:21:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post('/login', (req, res) => {
       signed: true, // For signed cookies
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
-    res.status(200).json({ message: 'Login successful' });
+    res.status(200).json({ message: 'Login successfuly' });
   } else {
     res.status(400).json({ message: 'Invalid username' });
   }
